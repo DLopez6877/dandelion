0947a1e9f39c4ebf799a72a9058b2b93ff53fa94
Daniel Lopez <dlopez6877@gmail.com>
2017-08-28 21:44:36 -0700

    change canvas size to 600, decrease clicks to 5, decrease new branch width, change functions to prototypes

--- js/branch.js
+++ js/branch.js
@@ -2,56 +2,76 @@ function Branch(begin, end) {
   this.begin = begin;
   this.end = end;
   this.finished = false;
-  this.angle = PI/random(3,7);
-  this.downAngle = this.angle + 70;
-  this.jitter = function() {
-    this.end.x += random(-1, 1);
-    this.end.y += random(-1, 1);
-  }
-  this.blowAway = function() {
-    this.end.x += random(0,17);
-    this.end.y += random(0,-8);
-  }
+  this.angle = PI/random(5,7);
+  this.middleAngle = this.angle + 70;
+  this.downAngle = this.angle + 140;
+}
 
-  this.show = function() {
-    stroke(255);
-    line(this.begin.x, this.begin.y, this.end.x, this.end.y);
-  }
+Branch.prototype.show = function () {
+  stroke(255);
+  line(this.begin.x, this.begin.y, this.end.x, this.end.y);
+};
 
-  this.branchLeft = function() {
-    var dir = p5.Vector.sub(this.end, this.begin);
-    dir.rotate(this.angle * -1);
-    dir.mult(0.67);
-    var newEnd = p5.Vector.add(this.end, dir);
-    var left = new Branch(this.end, newEnd);
-    return left;
-  }
+Branch.prototype.jitter = function () {
+  this.end.x += random(-1, 1);
+  this.end.y += random(-1, 1);
+};
 
-  this.branchRight = function() {
-    var dir = p5.Vector.sub(this.end, this.begin);
-    dir.rotate(this.angle);
-    dir.mult(0.67);
-    var newEnd = p5.Vector.add(this.end, dir);
-    var right = new Branch(this.end, newEnd);
-    return right;
-  }
+Branch.prototype.blowAway = function () {
+  this.end.x += random(0,6);
+  this.end.y += random(0,-6);
+};
 
-  this.branchBottomLeft = function() {
-    var dir = p5.Vector.sub(this.end, this.begin);
-    dir.rotate(this.downAngle * -1);
-    dir.mult(0.67);
-    var newEnd = p5.Vector.add(this.end, dir);
-    var left = new Branch(this.end, newEnd);
-    return left;
-  }
+Branch.prototype.branchLeft = function() {
+  var dir = p5.Vector.sub(this.end, this.begin);
+  dir.rotate(this.angle * -1);
+  dir.mult(0.6);
+  var newEnd = p5.Vector.add(this.end, dir);
+  var left = new Branch(this.end, newEnd);
+  return left;
+}
 
-  this.branchBottomRight = function() {
-    var dir = p5.Vector.sub(this.end, this.begin);
-    dir.rotate(this.downAngle);
-    dir.mult(0.67);
-    var newEnd = p5.Vector.add(this.end, dir);
-    var right = new Branch(this.end, newEnd);
-    return right;
-  }
+Branch.prototype.branchRight = function() {
+  var dir = p5.Vector.sub(this.end, this.begin);
+  dir.rotate(this.angle);
+  dir.mult(0.6);
+  var newEnd = p5.Vector.add(this.end, dir);
+  var right = new Branch(this.end, newEnd);
+  return right;
+}
 
+Branch.prototype.branchMiddleLeft = function() {
+  var dir = p5.Vector.sub(this.end, this.begin);
+  dir.rotate(this.middleAngle * -1);
+  dir.mult(0.6);
+  var newEnd = p5.Vector.add(this.end, dir);
+  var left = new Branch(this.end, newEnd);
+  return left;
+}
+
+Branch.prototype.branchMiddleRight = function() {
+  var dir = p5.Vector.sub(this.end, this.begin);
+  dir.rotate(this.middleAngle);
+  dir.mult(0.6);
+  var newEnd = p5.Vector.add(this.end, dir);
+  var right = new Branch(this.end, newEnd);
+  return right;
+}
+
+Branch.prototype.branchBottomLeft = function() {
+  var dir = p5.Vector.sub(this.end, this.begin);
+  dir.rotate(this.downAngle * -1);
+  dir.mult(0.6);
+  var newEnd = p5.Vector.add(this.end, dir);
+  var left = new Branch(this.end, newEnd);
+  return left;
+}
+
+Branch.prototype.branchBottomRight = function() {
+  var dir = p5.Vector.sub(this.end, this.begin);
+  dir.rotate(this.downAngle);
+  dir.mult(0.6);
+  var newEnd = p5.Vector.add(this.end, dir);
+  var right = new Branch(this.end, newEnd);
+  return right;
 }
--- js/sketch.js
+++ js/sketch.js
@@ -4,27 +4,29 @@ var counter = 0;
 
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(600, 600);
   var a = createVector(width / 2, height);
-  var b = createVector(width / 2, 550);
+  var b = createVector(width / 2, 320);
   var root = new Branch(a, b);
 
   dandelion[0] = root;
 }
 
 function mousePressed() {
-  if (counter < 6) {
+  if (counter < 4) {
     for (var i = dandelion.length - 1; i >= 0; i--) {
       if (!dandelion[i].finished) {
         dandelion.push(dandelion[i].branchLeft());
         dandelion.push(dandelion[i].branchRight());
+        dandelion.push(dandelion[i].branchMiddleLeft());
+        dandelion.push(dandelion[i].branchMiddleRight());
         dandelion.push(dandelion[i].branchBottomLeft());
         dandelion.push(dandelion[i].branchBottomRight());
       }
       dandelion[i].finished = true;
     }
   }
-  if (counter < 7) {
+  if (counter < 5) {
     counter++;
   }
 }
@@ -35,13 +37,13 @@ function draw() {
   for (var i = 0; i < dandelion.length; i++) {
     if (counter <= 3){
       dandelion[i].show();
-    } else if (counter > 3 && counter != 7) {
+    } else if (counter > 3 && counter != 5) {
       dandelion[i].jitter();
       dandelion[i].show();
-    } else if (counter === 7 && i > 4){
+    } else if (counter === 5 && i > 6){
       dandelion[i].show();
       dandelion[i].blowAway();
-    } else if (counter === 7 && i > 0){
+    } else if (counter === 5 && i > 0){
       dandelion[i].blowAway();
     } else {
       dandelion[0].show();
@@ -51,7 +53,7 @@ function draw() {
         dandelion[0].end.y -= 2;
         dandelion[0].begin.y -= 5;
         dandelion[0].begin.x += 4;
-      },3000);
+      },1500);
 
     }
   }
